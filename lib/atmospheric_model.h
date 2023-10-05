d9f711296b91f944c420beff163ee460de9b91b0
VariPor <maiorova.ev@phystech.edu>
2023-10-05 23:07:13 +0300

    math: creating abstract class AtmosphericModel
    pure virtual function:
    for slant range from aircraft to surface target via declination angle and height
    for slant range from aircraft to surface target via sliding angle and height
    the length of the arc on the Earth's surface between the nadir direction of the radar and purpose
    for declination angle via slant range, heights of the radar and purpose
    for sliding angle via slant range, heights of the radar and purpose
    for angular measure of the arc of the Earth's surface from the radar to the target
    for height of purpose above sea level via declination angle, height of the radar, slant range

---
+++ lib/atmospheric_model.h
@@ -0,0 +1,33 @@
+﻿#ifndef ATMOSPHERIC_MODEL_H
+#define ATMOSPHERIC_MODEL_H
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+// radius of the Earth in meters
+const double Re = 6378000;
+
+struct AtmosphericModel {
+    // Formula for slant range from aircraft to surface target via declination angle and heights
+    virtual double R_via_psi_d(double ha, double hs, double psi_d, void *opaque) = 0;
+
+    // Formula for slant range from aircraft to surface target via sliding angle and height
+    virtual double R_via_psi_g(double ha, double hs, double psi_g, void *opaque) = 0;
+
+    // Formula for length of the arc on the Earth's surface between the nadir direction of the radar and purpose
+    virtual double d(double psi_d, double psi_g, void *opaque) = 0;
+
+    // Formula for declination angle via slant range, heights of the radar and purpose
+    virtual double psi_d(double ha, double hs, double R, void *opaque) = 0;
+
+    // Formula for sliding angle via slant range, heights of the radar and purpose
+    virtual double psi_g(double ha, double hs, double R, void *opaque) = 0;
+
+    // Formula for angular measure of the arc of the Earth's surface from the radar to the target
+    virtual double phi_e(double ha, double hs, double R, void *opaque) = 0;
+
+    // Formula for height of purpose above sea level via declination angle, height of the radar, slant range
+    virtual double hs_via_psi_d(double ha, double psi_d, double R, void *opaque) = 0;
+};
+
+#endif // ATMOSPHERIC_MODEL_H
