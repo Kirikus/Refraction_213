44ad96576557bcdddc19dc8dfb87d50d45e1b101
VariPor <maiorova.ev@phystech.edu>
2023-10-07 15:17:15 +0300

    math: correction with clang format

--- lib/model_without_refraction.cpp
+++ lib/model_without_refraction.cpp
@@ -1,47 +1,54 @@
 #include "model_without_refraction.h"
 
-double ModelWithoutRefraction::R_via_psi_d(double ha, double hs, double psi_d, void* opaque)
-{
-    double discriminant = pow((Re + ha) * std::sin(psi_d), 2) - 2 * Re * (ha - hs) - ha * ha + hs * hs;
-    double R = (Re + ha) * std::sin(psi_d) - std::sqrt(discriminant);
-    return R;
+double ModelWithoutRefraction::R_via_psi_d(double ha, double hs, double psi_d,
+                                           void* opaque) {
+  double discriminant = pow((Re + ha) * std::sin(psi_d), 2) -
+                        2 * Re * (ha - hs) - ha * ha + hs * hs;
+  double R = (Re + ha) * std::sin(psi_d) - std::sqrt(discriminant);
+  return R;
 }
 
-double ModelWithoutRefraction::R_via_psi_g(double ha, double hs, double psi_g, void* opaque)
-{
-    double discriminant = pow((Re + hs) * std::sin(psi_g), 2) + 2 * Re * ha + ha * ha - 2 * hs * Re - hs * hs;
-    double R = -(Re + hs) * std::sin(psi_g) + std::sqrt(discriminant);
-    return R;
+double ModelWithoutRefraction::R_via_psi_g(double ha, double hs, double psi_g,
+                                           void* opaque) {
+  double discriminant = pow((Re + hs) * std::sin(psi_g), 2) + 2 * Re * ha +
+                        ha * ha - 2 * hs * Re - hs * hs;
+  double R = -(Re + hs) * std::sin(psi_g) + std::sqrt(discriminant);
+  return R;
 }
 
-double ModelWithoutRefraction::d(double psi_d, double psi_g, void* opaque)
-{
-    double d = Re * (psi_d - psi_g);
-    return d;
+double ModelWithoutRefraction::d(double psi_d, double psi_g, void* opaque) {
+  double d = Re * (psi_d - psi_g);
+  return d;
 }
 
-double ModelWithoutRefraction::psi_d(double ha, double hs, double R, void* opaque)
-{
-    if (R == 0) return 0;   // if purpose and radar in same dote
-    double psi_d = std::asin((R * R + ha * ha + 2 * ha * Re - hs * hs - 2 * hs * Re) / (2 * R * (Re + ha)));
-    return psi_d;
+double ModelWithoutRefraction::psi_d(double ha, double hs, double R,
+                                     void* opaque) {
+  if (R == 0) return 0;  // if purpose and radar in same dote
+  double psi_d =
+      std::asin((R * R + ha * ha + 2 * ha * Re - hs * hs - 2 * hs * Re) /
+                (2 * R * (Re + ha)));
+  return psi_d;
 }
 
-double ModelWithoutRefraction::psi_g(double ha, double hs, double R, void* opaque)
-{
-    if (R == 0) return 0;   // if purpose and radar in same dote
-    double psi_g = std::asin((R * R - ha * ha - 2 * ha * Re + hs * hs + 2 * hs * Re) / (2 * R * (Re + hs)));
-    return psi_g;
+double ModelWithoutRefraction::psi_g(double ha, double hs, double R,
+                                     void* opaque) {
+  if (R == 0) return 0;  // if purpose and radar in same dote
+  double psi_g =
+      std::asin((R * R - ha * ha - 2 * ha * Re + hs * hs + 2 * hs * Re) /
+                (2 * R * (Re + hs)));
+  return psi_g;
 }
 
-double ModelWithoutRefraction::phi_e(double ha, double hs, double R, void* opaque)
-{
-    double phi_e = psi_d(ha, hs, R) - psi_g(ha, hs, R);
-    return phi_e;
+double ModelWithoutRefraction::phi_e(double ha, double hs, double R,
+                                     void* opaque) {
+  double phi_e = psi_d(ha, hs, R) - psi_g(ha, hs, R);
+  return phi_e;
 }
 
-double ModelWithoutRefraction::hs_via_psi_d(double ha, double psi_d, double R, void* opaque)
-{
-    double hs = std::sqrt(R * R + pow(ha + Re, 2) - 2 * R * (ha + Re) * std::sin(psi_d)) - Re;
-    return hs;
+double ModelWithoutRefraction::hs_via_psi_d(double ha, double psi_d, double R,
+                                            void* opaque) {
+  double hs =
+      std::sqrt(R * R + pow(ha + Re, 2) - 2 * R * (ha + Re) * std::sin(psi_d)) -
+      Re;
+  return hs;
 }
--- lib/model_without_refraction.h
+++ lib/model_without_refraction.h
@@ -4,69 +4,77 @@
 #include "refraction_model.h"
 
 struct ModelWithoutRefraction : public RefractionModel {
+  // Formula for slant range from aircraft to surface target via declination
+  // angle and heights
+  // SRC: (2.10) from citation, image (2.30), inferential
+  //   R_via_psi_d : lant range from aircraft to surface target, m
+  //   ha : height of aircraft above sea level, m
+  //   hs : height of purpose above sea level, m
+  //   psi_d : declination angle, radians
+  //   opaque : not used
+  virtual double R_via_psi_d(double ha, double hs, double psi_d,
+                             void* opaque = nullptr) override;
 
-    // Formula for slant range from aircraft to surface target via declination angle and heights
-    // SRC: (2.10) from citation, image (2.30), inferential
-    //   R_via_psi_d : lant range from aircraft to surface target, m
-    //   ha : height of aircraft above sea level, m
-    //   hs : height of purpose above sea level, m
-    //   psi_d : declination angle, radians
-    //   opaque : not used
-    virtual double R_via_psi_d(double ha, double hs, double psi_d, void* opaque = nullptr) override;
+  // Formula for slant range from aircraft to surface target via sliding angle
+  // and height
+  // SRC: (2.10) from citation
+  //   R_via_psi_g : slant range from aircraft to surface target, m
+  //   ha : height of aircraft above sea level, m
+  //   hs : height of purpose above sea level, m
+  //   psi_g : sliding angle, radians
+  //   opaque : not used
+  virtual double R_via_psi_g(double ha, double hs, double psi_g,
+                             void* opaque = nullptr) override;
 
-    // Formula for slant range from aircraft to surface target via sliding angle and height
-    // SRC: (2.10) from citation
-    //   R_via_psi_g : slant range from aircraft to surface target, m
-    //   ha : height of aircraft above sea level, m
-    //   hs : height of purpose above sea level, m
-    //   psi_g : sliding angle, radians
-    //   opaque : not used
-    virtual double R_via_psi_g(double ha, double hs, double psi_g, void* opaque = nullptr) override;
+  // Formula for length of the arc on the Earth's surface between the nadir
+  // direction of the radar and purpose
+  // SRC: (2.9, 2.11) from citation
+  //   d : the length of the arc on the Earth's surface between the nadir
+  //   direction of the radar and purpose, m psi_d : declination angle, radians
+  //   psi_g : sliding angle, radians
+  //   opaque : not used
+  virtual double d(double psi_d, double psi_g, void* opaque = nullptr) override;
 
-    // Formula for length of the arc on the Earth's surface between the nadir direction of the radar and purpose
-    // SRC: (2.9, 2.11) from citation
-    //   d : the length of the arc on the Earth's surface between the nadir direction of the radar and purpose, m
-    //   psi_d : declination angle, radians
-    //   psi_g : sliding angle, radians
-    //   opaque : not used
-    virtual double d(double psi_d, double psi_g, void* opaque = nullptr) override;
+  // Formula for declination angle via slant range, heights of the radar and
+  // purpose
+  // SRC: image (2.30), inferential
+  //   psi_d : declination angle, radians
+  //   ha : height of aircraft above sea level, m
+  //   hs : height of purpose above sea level, m
+  //   R : slant range from aircraft to surface target, m
+  //   opaque : not used
+  virtual double psi_d(double ha, double hs, double R,
+                       void* opaque = nullptr) override;
 
-    // Formula for declination angle via slant range, heights of the radar and purpose
-    // SRC: image (2.30), inferential
-    //   psi_d : declination angle, radians
-    //   ha : height of aircraft above sea level, m
-    //   hs : height of purpose above sea level, m
-    //   R : slant range from aircraft to surface target, m
-    //   opaque : not used
-    virtual double psi_d(double ha, double hs, double R, void* opaque = nullptr) override;
+  // Formula for sliding angle via slant range, heights of the radar and purpose
+  // SRC: image (2.30), inferential
+  //   psi_g : sliding angle, radians
+  //   ha : height of aircraft above sea level, m
+  //   hs : height of purpose above sea level, m
+  //   R : slant range from aircraft to surface target, m
+  //   opaque : not used
+  virtual double psi_g(double ha, double hs, double R,
+                       void* opaque = nullptr) override;
 
-    // Formula for sliding angle via slant range, heights of the radar and purpose
-    // SRC: image (2.30), inferential
-    //   psi_g : sliding angle, radians
-    //   ha : height of aircraft above sea level, m
-    //   hs : height of purpose above sea level, m
-    //   R : slant range from aircraft to surface target, m
-    //   opaque : not used
-    virtual double psi_g(double ha, double hs, double R, void* opaque = nullptr) override;
+  // Formula for angular measure of the arc of the Earth's surface from the
+  // radar to the target
+  // SRC: (2.9) from citation
+  //   psi_g : angular measure of the arc of the Earth's surface from the radar
+  //   to the target, radians ha : height of aircraft above sea level, m hs :
+  //   height of purpose above sea level, m R : slant range from aircraft to
+  //   surface target, m opaque : not used
+  virtual double phi_e(double ha, double hs, double R,
+                       void* opaque = nullptr) override;
 
-    // Formula for angular measure of the arc of the Earth's surface from the radar to the target
-    // SRC: (2.9) from citation
-    //   psi_g : angular measure of the arc of the Earth's surface from the radar to the target, radians
-    //   ha : height of aircraft above sea level, m
-    //   hs : height of purpose above sea level, m
-    //   R : slant range from aircraft to surface target, m
-    //   opaque : not used
-    virtual double phi_e(double ha, double hs, double R, void* opaque = nullptr) override;
-
-    // Formula for height of purpose above sea level via declination angle, height of the radar, slant range
-    // SRC: image (2.30), inferential
-    //   hs_via_psi_d : height of purpose above sea level via declination angle, height of the radar, slant range, m
-    //   ha : height of aircraft above sea level, m
-    //   psi_d : declination angle, radians
-    //   R : slant range from aircraft to surface target, m
-    //   opaque : not used
-    virtual double hs_via_psi_d(double ha, double psi_d, double R, void* opaque = nullptr) override;
-
+  // Formula for height of purpose above sea level via declination angle, height
+  // of the radar, slant range
+  // SRC: image (2.30), inferential
+  //   hs_via_psi_d : height of purpose above sea level via declination angle,
+  //   height of the radar, slant range, m ha : height of aircraft above sea
+  //   level, m psi_d : declination angle, radians R : slant range from aircraft
+  //   to surface target, m opaque : not used
+  virtual double hs_via_psi_d(double ha, double psi_d, double R,
+                              void* opaque = nullptr) override;
 };
 
-#endif // MODEL_WITHOUT_REFRACTION_H
+#endif  // MODEL_WITHOUT_REFRACTION_H
