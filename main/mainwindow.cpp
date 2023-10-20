0384583fce45dd0adb7214c98ba56081e0afc38b
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-10-20 18:25:15 +0300

    added slots for QLineEdits and changed placeholder texsts
    changed placeholder texts for LineEdits, where user inputs height of station, height of target and distance between station and target
    
    added slots for these LineEdits, so I will be able to fill user_input_data fields

--- main/mainwindow.cpp
+++ main/mainwindow.cpp
@@ -1,33 +1,21 @@
 #include "mainwindow.h"
-#include "QMessageBox"
-#include <iostream>
+#include "data.h"
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
-  ui->stationCoordinatesEdit->setPlaceholderText("Координаты Станции");
-  ui->targetCoordinatesEdit->setPlaceholderText("Координаты Цели");
-  ui->distanceToTargetEdit->setPlaceholderText("Расстояние До Цели");
-
+  ui->stationCoordinatesEdit->setPlaceholderText("100");
+  ui->targetCoordinatesEdit->setPlaceholderText("1000");
+  ui->distanceToTargetEdit->setPlaceholderText("1400");
+  ui -> stationCoordinatesEdit -> setValidator(new QIntValidator());
+  ui -> targetCoordinatesEdit -> setValidator(new QIntValidator());
+  ui -> distanceToTargetEdit -> setValidator(new QIntValidator());
+  ui -> plot -> xAxis -> setRange(0, 10000);
+  ui -> plot -> yAxis -> setRange(0, 10000);
+  user_input_data.setPlot(ui -> plot);
+  
 }
 
 MainWindow::~MainWindow() { delete ui; }
-
-void MainWindow::on_reverseTaskButton_clicked()
-{
-    QMessageBox msg;
-    msg.setWindowTitle("reverse task");
-    msg.setText("I was pushed");
-    msg.exec();
-}
-
-
-void MainWindow::on_directTaskButton_clicked()
-{
-    QMessageBox msg;
-    msg.setWindowTitle("direct task");
-    msg.setText("I was pushed");
-    msg.exec();
-}
 
--- main/mainwindow.h
+++ main/mainwindow.h
@@ -12,15 +12,24 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow {
   Q_OBJECT
 
- public:
-  MainWindow(QWidget *parent = nullptr);
-  ~MainWindow();
+  public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
 
-private slots:
-  void on_directTaskButton_clicked();
+  private slots:
+    void on_stationCoordinatesEdit_textEdited(const QString & arg1);
 
-private slots:
-  void on_reverseTaskButton_clicked();
+  private slots:
+    void on_targetCoordinatesEdit_textEdited(const QString & arg1);
+
+  private slots:
+    void on_distanceToTargetEdit_textEdited(const QString & arg1);
+
+  private slots:
+    void on_directTaskButton_clicked();
+
+  private slots:
+    void on_reverseTaskButton_clicked();
 
 private:
   Ui::MainWindow *ui;
