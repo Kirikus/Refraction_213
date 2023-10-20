faf33ecd346fdd71c7f6215e7e62abd4bf21a5ac
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-10-20 18:46:08 +0300

    added buttons.cpp
    added buttons.cpp file for functions that process user actions like pushing buttons:
    
    on_reverseTaskButton_clicked()  --  plug
    on_directTaskButton_clicked()  --  plug

--- main/buttons.cpp
+++ main/buttons.cpp
@@ -18,18 +18,3 @@ void MainWindow::on_directTaskButton_clicked() {
   msg.exec();
 }
 
-void MainWindow::on_distanceToTargetEdit_textEdited(const QString & dst_to_target) {
-  user_input_data.setDstToTarget(dst_to_target.toInt());
-  addTargetAndStation();
-}
-
-void MainWindow::on_targetCoordinatesEdit_textEdited(const QString & trg_coords) {
-  user_input_data.setTrgCoords(trg_coords.toInt());
-  addTargetAndStation();
-}
-
-void MainWindow::on_stationCoordinatesEdit_textEdited(const QString & st_coords) {
-  user_input_data.setStCoords(st_coords.toInt());
-  addTargetAndStation();
-}
-
