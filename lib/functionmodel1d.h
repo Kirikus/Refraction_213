bc89a22893c48c5b5739fed6ddd38d2142073308
c4llery <anton.rusanov.04@gmail.com>
2023-11-01 22:52:38 +0300

    create FunctionModel1D class
    Fields:
    data - points to interpolate with
    
    Functions:
    y() - return the value based on interpolation
    fromCSV() - file to read points from

---
+++ lib/functionmodel1d.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONMODEL1D_H
+#define FUNCTIONMODEL1D_H
+#include <map>
+using std::map;
+
+class FunctionModel1D {
+ public:
+  virtual double y(double x) = 0;
+  virtual void fromCSV() = 0;
+
+ private:
+  map<double, double> data;
+};
+
+#endif  // FUNCTIONMODEL1D_H
