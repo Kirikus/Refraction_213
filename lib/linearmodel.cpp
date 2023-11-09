e6a8d66b54281e383bd1b637a92735c4c153831a
c4llery <anton.rusanov.04@gmail.com>
2023-11-10 02:33:49 +0300

    add struct IntervalData
    -Added struct IntervalData to LinearModel to conatin information about intervals
    Fields
    -first_x - start of interval
    -second_x - finish of interval
    -k and b - coefficients
    -calculated - checks whether interval is counted or not
    
    -Added vector of IntervalData
    -Added changes to y(x)

--- lib/linearmodel.cpp
+++ lib/linearmodel.cpp
@@ -9,6 +9,11 @@ double LinearModel::y(double x) {
   finish--;
   if (x <= (start->x)) return start->y;
   if (x >= (finish->x)) return finish->y;
+  for (int i = 0; i < interval_data.size(); ++i) {
+    auto interval = interval_data[i];
+    if (x >= interval.first_x && x <= interval.second_x)
+      return interval.k * x + interval.b;
+  }
   auto second_point = upper_bound(data.begin(), data.end(), x,
                                   [](int x, Point& z) { return x < z.x; });
   auto first_point = second_point;
@@ -19,6 +24,7 @@ double LinearModel::y(double x) {
   double y2 = (second_point)->y;
   double k = (y2 - y1) / (x2 - x1);
   double b = y1 - k * x1;
+  interval_data.push_back(IntervalData(x1, x2, k, b));
   return k * x + b;
 }
 void LinearModel::fromCSV() {}
--- lib/linearmodel.h
+++ lib/linearmodel.h
@@ -13,6 +13,16 @@ class LinearModel : public FunctionModel1D {
 
  private:
   vector<Point> data;
+  struct IntervalData {
+    double first_x;
+    double second_x;
+    double k;
+    double b;
+    bool calculated{false};
+    IntervalData(double x1, double x2, double k1, double b1)
+        : first_x{x1}, second_x{x2}, k{k1}, b{b1}, calculated{true} {};
+  };
+  vector<IntervalData> interval_data;
 };
 
 #endif  // LINEARMODEL_H
