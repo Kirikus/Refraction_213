a642c462044420d523d695d683762afd4a1e88f1
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-11-16 19:18:09 +0300

    gui: added maximum height for LineEdits

--- main/mainwindow.cpp
+++ main/mainwindow.cpp
@@ -5,15 +5,16 @@
 #include "doubleValidator.h"
 
 constexpr double max_distance = 12800000;
+constexpr double max_height = 50000;
 constexpr double max_decimals = 5;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
   ui->stationCoordinatesEdit->setValidator(new betterDoubleValidator(
-      0, max_distance, max_decimals, ui->stationCoordinatesEdit));
+      0, max_height, max_decimals, ui->stationCoordinatesEdit));
   ui->targetCoordinatesEdit->setValidator(new betterDoubleValidator(
-      0, max_distance, max_decimals, ui->targetCoordinatesEdit));
+      0, max_height, max_decimals, ui->targetCoordinatesEdit));
   ui->distanceToTargetEdit->setValidator(new betterDoubleValidator(
       0, max_distance, max_decimals, ui->distanceToTargetEdit));
   ui->plot->xAxis->setRange(0, 10000);
