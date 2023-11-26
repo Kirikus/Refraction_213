46efbad3435fe629501a9cf0950c609766318df2
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-11-26 13:43:10 +0300

    gui: added extra fields and functions in Data
    some models need extra data to work with, so now it will be stored in Data
    we have:
    1) height_of_surface -- the field for segmented and exponential atmospheric models
    2) refractive_index_near_surface -- the field for segmented and exponential atmospheric models
    3) counting_method -- the field needed for averageK and averageP refraction models
    
    also added enum classes for atmospheric and refraction models. It's easier to understand in code which model is atmospheric and which is refraction
    
    the last but not the least - added functions to get and set  values of all the fields

--- main/data.h
+++ main/data.h
@@ -4,20 +4,29 @@
 #include <qcustomplot.h>
 
 #include <cmath>
+#include <map>
 #include <string>
 
 namespace gui {
-
+enum CountingMethod { Fitting, Integration };
 enum Task { Forward, Reversed };
-enum AtmosphericModel { GOST440481, Segmented, Numerical_Integration };
-enum RefractionModel { Effective_Radius43, Geometric, AverageK, AverageRho };
+enum class AtmosphericModel { GOST440481, Segmented, Exponential };
+enum class RefractionModel {
+  Effective_Radius43,
+  Geometric,
+  AverageK,
+  AverageRho
+};
 
 class Data {
   double target_height = std::nan("1");
   double station_height = std::nan("1");
   double dist_to_target = std::nan("1");
-  AtmosphericModel atmospheric_model = GOST440481;
-  RefractionModel refraction_model = Geometric;
+  double height_of_surface = std::nan("1");
+  double refractive_index_near_surface = std::nan("1");
+  CountingMethod counting_method = Fitting;
+  AtmosphericModel atmospheric_model = AtmosphericModel::GOST440481;
+  RefractionModel refraction_model = RefractionModel::Geometric;
   Task task = Forward;
 
  public:
@@ -26,16 +35,25 @@ class Data {
        AtmosphericModel atmospheric_model, RefractionModel refraction_model,
        Task task);
   Data();
+  void setHeightOfSurface(double height);
+  void setRefractiveIndex(double index);
+  void setCountingMethod(CountingMethod method);
   void setPlot(QCustomPlot *plt);
   void setStation(double height);
   void setTarget(double height);
   void setDistance(double distance_from_station_to_target);
   void setAtmosphericModel(AtmosphericModel atmospheric_model);
   void setRefractionModel(RefractionModel refraction_model);
-  void setTask(enum Task);
+  void setTask(Task task);
   double getStation();
   double getTarget();
   double getDistance();
+  double getHeightOfSurface();
+  double getRefractiiveIndex();
+  CountingMethod getCountingMethod();
+  AtmosphericModel getAtmosphericModel();
+  RefractionModel getRefractionModel();
+  Task getTask();
 };
 }  // namespace gui
 extern gui::Data user_input_data;
