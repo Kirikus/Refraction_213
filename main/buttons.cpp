46f627481afc0a306339ef7744ae190b93160c2e
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-11-28 20:56:53 +0300

    gui: removed self-made parsing
    Now I can use standard QString method doDouble

--- main/buttons.cpp
+++ main/buttons.cpp
@@ -2,7 +2,6 @@
 
 #include "data.h"
 #include "mainwindow.h"
-#include "parsing.h"
 #include "plotting.h"
 
 void MainWindow::on_reverseTaskButton_clicked() {
@@ -21,18 +20,18 @@ void MainWindow::on_directTaskButton_clicked() {
 
 void MainWindow::on_distanceToTargetEdit_textEdited(
     const QString &dst_to_target) {
-  user_input_data.setDistance(parse_double(dst_to_target.toStdString()));
+  user_input_data.setDistance(dst_to_target.toDouble());
   addTargetAndStation();
 }
 
 void MainWindow::on_targetCoordinatesEdit_textEdited(
     const QString &trg_height) {
-  user_input_data.setTarget(parse_double(trg_height.toStdString()));
+  user_input_data.setTarget(trg_height.toDouble());
   addTargetAndStation();
 }
 
 void MainWindow::on_stationCoordinatesEdit_textEdited(
     const QString &st_height) {
-  user_input_data.setStation(parse_double(st_height.toStdString()));
+  user_input_data.setStation(st_height.toDouble());
   addTargetAndStation();
 }
