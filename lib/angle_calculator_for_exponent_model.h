c7bb6fa8301de93374fbc85f8bcfbff4ea7b276e
VariPor <maiorova.ev@phystech.edu>
2023-11-29 19:34:37 +0300

    math: small fix

--- lib/angle_calculator_for_exponent_model.h
+++ lib/angle_calculator_for_exponent_model.h
@@ -5,6 +5,7 @@
 #include "exponent_model.h"
 
 class AngleCalculatorForExponentModel : public AngleCalculator {
+ public:
   // Formula for declination angle
   // SRC: (2.44) from citation
   //    psi_d : declination angle, radians
--- lib/universal_angle_calculator.h
+++ lib/universal_angle_calculator.h
@@ -4,6 +4,7 @@
 #include "angle_calculator.h"
 
 class UniversalAngleCalculator : public AngleCalculator {
+ public:
   // Formula for declination angle
   // SRC: (2.42) from citation
   //    psi_d : declination angle, radians
