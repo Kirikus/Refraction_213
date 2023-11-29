ae980e2dd04fd3c2e5f42be171d8d4bb88eed08c
VariPor <maiorova.ev@phystech.edu>
2023-11-29 21:57:40 +0300

    math: fix formulas for angle calculator

--- lib/universal_angle_calculator.cpp
+++ lib/universal_angle_calculator.cpp
@@ -3,13 +3,23 @@
 #include "refraction_model.h"
 
 double UniversalAngleCalculator::psi_d(double ha, double hs, double psi_g) {
-  double cos_psi_d = std::cos(psi_g) * atmosphere->N(hs) / atmosphere->N(ha) *
-                     (Re + hs) / (Re + ha);
+  double integral = 0;
+  double N = 1000;
+  double dh = (ha - hs) / 1000;
+  for (int i = 1; i < N; ++i)
+    integral += 1 / atmosphere->N(hs + dh * i) *
+                (atmosphere->N(hs + dh * i) - atmosphere->N(hs + dh * (i - 1)));
+  double cos_psi_d = std::cos(psi_g) * exp(-integral) * (Re + hs) / (Re + ha);
   return std::acos(cos_psi_d);
 }
 
 double UniversalAngleCalculator::psi_g(double ha, double hs, double psi_d) {
-  double cos_psi_g = std::cos(psi_d) * atmosphere->N(ha) / atmosphere->N(hs) *
-                     (Re + ha) / (Re + hs);
+  double integral = 0;
+  double N = 1000;
+  double dh = (ha - hs) / 1000;
+  for (int i = 1; i < N; ++i)
+    integral += 1 / atmosphere->N(hs + dh * i) *
+                (atmosphere->N(hs + dh * i) - atmosphere->N(hs + dh * (i - 1)));
+  double cos_psi_g = std::cos(psi_d) * exp(integral) * (Re + ha) / (Re + hs);
   return std::acos(cos_psi_g);
 }
