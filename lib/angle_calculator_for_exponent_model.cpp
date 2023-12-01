cc8fd7ae4d85cb2eacb8bae45f28e3d2fcd7a4d5
VariPor <maiorova.ev@phystech.edu>
2023-12-01 13:31:09 +0300

    math: fix formulas for angle calculator

--- lib/angle_calculator_for_exponent_model.cpp
+++ lib/angle_calculator_for_exponent_model.cpp
@@ -4,8 +4,9 @@ double AngleCalculatorForExponentModel::psi_d(double ha, double hs,
                                               double psi_g) {
   double Ns = atmosphere.get_Ns();
   double Hb = atmosphere.get_Hb();
-  double cos_psi_d = ((Ns) / (Ns * exp(-(ha - hs) / Hb))) *
-                     ((Re + hs) / (Re + ha)) * std::cos(psi_g);
+  double cos_psi_d =
+      ((1 + pow(10, -6) * Ns) / (1 + pow(10, -6) * Ns * exp(-(ha - hs) / Hb))) *
+      ((Re + hs) / (Re + ha)) * std::cos(psi_g);
   return std::acos(cos_psi_d);
 }
 
@@ -13,7 +14,8 @@ double AngleCalculatorForExponentModel::psi_g(double ha, double hs,
                                               double psi_d) {
   double Ns = atmosphere.get_Ns();
   double Hb = atmosphere.get_Hb();
-  double cos_psi_g = ((Ns * exp(-(ha - hs) / Hb)) / (Ns)) *
-                     ((Re + ha) / (Re + hs)) * std::cos(psi_d);
+  double cos_psi_g =
+      ((1 + pow(10, -6) * Ns * exp(-(ha - hs) / Hb)) / (1 + pow(10, -6) * Ns)) *
+      ((Re + ha) / (Re + hs)) * std::cos(psi_d);
   return std::acos(cos_psi_g);
 }
