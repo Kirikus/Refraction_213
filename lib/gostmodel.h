96d952a34ae031f64684d1ea4c8c79d08e47b2e1
c4llery <anton.rusanov.04@gmail.com>
2023-12-06 19:07:09 +0300

    fix gostmodel.h
    Added inheritance from AtmospericModel

--- lib/gostmodel.h
+++ lib/gostmodel.h
@@ -1,12 +1,13 @@
 #ifndef GOSTMODEL_H
 #define GOSTMODEL_H
 #include <../lib/functionmodel1d.h>
+#include <atmospheric_model.h>
 
-class GOSTModel {
+class GOSTModel: public AtmosphericModel {
  public:
   GOSTModel(FunctionModel1D* first_model, FunctionModel1D* second_model)
       : p_via_h{first_model}, T_via_h{second_model} {}
-  double N(double h);
+  double N(double h) override;
   double Ro(double);
  private:
   FunctionModel1D* p_via_h;
