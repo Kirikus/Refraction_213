025fe317e8f1d239967027e172deec2bdc7c7d72
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-12-08 21:44:02 +0300

    math: changed GeometricModelLine
    removed abs() from calculate function as it causes wrong calculations, when angles should be negative

--- lib/geometric_model_line.cpp
+++ lib/geometric_model_line.cpp
@@ -3,7 +3,7 @@
 RefractionModel::Answer GeometricModelLine::calculate(
     const RefractionModel::Input &data, void *opaque) {
   if (data.R == 0) return Answer{.psi_d = 0, .psi_g = 0, .d = 0};
-  double psi = std::asin(abs(data.ha - data.hs) / data.R);
+  double psi = std::asin((data.ha - data.hs) / data.R);
   double d = data.R * std::cos(psi);
   return Answer{.psi_d = psi, .psi_g = psi, .d = d};
 }
